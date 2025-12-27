c94a2f9c343295e7c37ce684ac7a2e7a1bbd71fb
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 01:04:01 +0100

    Added query middleware excluding inactive users from find queries

--- models/userModel.js
+++ models/userModel.js
@@ -78,6 +78,11 @@ userSchema.pre("save", function (next) {
   next();
 });
 
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
+  next();
+});
+
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
