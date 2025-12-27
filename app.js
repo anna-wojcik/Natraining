ab00a0f820a64db2b9ccd77149b4c91e61521f0e
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 14:36:20 +0100

    Add hpp middleware for preventing parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -39,6 +40,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "level",
+      "price",
+    ],
+  })
+);
 
 app.set("view engine", "pug"); // template engine - pug
 app.set("views", path.join(__dirname, "views"));
