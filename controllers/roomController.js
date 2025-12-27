9cfce009cd2406ed6d663d8a52d9ea43dd11af53
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 23:12:41 +0100

    Use factory handlers for Room CRUD operations

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,68 +1,8 @@
 const Room = require("../models/roomModel");
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
+const factory = require("./handlerFactory");
 
-exports.getAllRooms = catchAsync(async (req, res, next) => {
-  const rooms = await Room.find();
-
-  // + Filter np. available
-
-  res.status(200).json({
-    status: "success",
-    results: rooms.length,
-    data: {
-      rooms,
-    },
-  });
-});
-
-exports.getRoom = catchAsync(async (req, res, next) => {
-  const room = await Room.findById(req.params.id);
-
-  if (!room) return next(new AppError("No room found with that ID", 404));
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      room,
-    },
-  });
-});
-
-exports.createRoom = catchAsync(async (req, res, next) => {
-  const room = await Room.create(req.body);
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      room,
-    },
-  });
-});
-
-exports.updateRoom = catchAsync(async (req, res, next) => {
-  const room = await Room.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!room) return next(new AppError("No room found with that ID", 404));
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      room,
-    },
-  });
-});
-
-exports.deleteRoom = catchAsync(async (req, res, next) => {
-  const room = await Room.findByIdAndDelete(req.params.id);
-
-  if (!room) return next(new AppError("No room found with that ID", 404));
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-});
+exports.getAllRooms = factory.getAll(Room);
+exports.getRoom = factory.getOne(Room);
+exports.createRoom = factory.createOne(Room);
+exports.updateRoom = factory.updateOne(Room);
+exports.deleteRoom = factory.deleteOne(Room);
