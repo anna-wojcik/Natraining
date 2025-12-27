fa30e447c102ecb3226abaeeb9e198930a8fa9a0
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 13:08:38 +0100

    Add helmet middleware for HTTP header security, add limit request body size to 10kb to prevent DoS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,10 +13,15 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+// Set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100, // max 100 request
   windowMs: 60 * 60 * 1000, // in 1 hour
@@ -24,16 +30,12 @@ const limiter = rateLimit({
 app.use("/api", limiter); // limiter works only if URL contains /api
 
 // Body parser, reading data from the body into req.body
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 app.set("view engine", "pug"); // template engine - pug
 app.set("views", path.join(__dirname, "views"));
 
-// app.use((req, res, next) => {
-//   console.log("Hello from the middleware");
-//   next();
-// });
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
