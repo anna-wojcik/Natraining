6803bf36b186bcb517b3b6c7ae7963db6b9148f7
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 23:19:29 +0100

    Use factory handlers for Training CRUD operations

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -1,72 +1,8 @@
 const Training = require("../models/trainingModel");
-const APIFeatures = require("../utils/apiFeatures");
-const AppError = require("../utils/appError");
-const catchAsync = require("../utils/catchAsync");
+const factory = require("./handlerFactory");
 
-exports.getAllTrainings = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Training.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const trainings = await features.query;
-
-  res.status(200).json({
-    status: "success",
-    results: trainings.length,
-    data: {
-      trainings,
-    },
-  });
-});
-
-exports.getTraining = catchAsync(async (req, res, next) => {
-  const training = await Training.findById(req.params.id);
-
-  if(!training) return next(new AppError('No training found with that ID', 404));
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      training,
-    },
-  });
-});
-
-exports.createTraining = catchAsync(async (req, res, next) => {
-  const training = await Training.create(req.body);
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      training,
-    },
-  });
-});
-
-exports.updateTraining = catchAsync(async (req, res, next) => {
-  const training = await Training.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if(!training) return next(new AppError('No training found with that ID', 404));
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      training,
-    },
-  });
-});
-
-exports.deleteTraining = catchAsync(async (req, res, next) => {
-  const training = await Training.findByIdAndDelete(req.params.id);
-
-  if(!training) return next(new AppError('No training found with that ID', 404));
-  
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-});
+exports.getAllTrainings = factory.getAll(Training);
+exports.getTraining = factory.getOne(Training);
+exports.createTraining = factory.createOne(Training);
+exports.updateTraining = factory.updateOne(Training);
+exports.deleteTraining = factory.deleteOne(Training);
