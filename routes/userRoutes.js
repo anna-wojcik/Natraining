3c47296d8654ea0e74d4fe931e828d89f44a8088
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-28 00:04:27 +0100

    Protect user routes and restrict user management to admins

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,12 +9,13 @@ router.post("/login", authController.login);
 
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
-router
-  .route("/updateMyPassword")
-  .patch(authController.protect, authController.updatePassword);
-router.patch("/updateMe", authController.protect, userController.updateMe);
-router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
+router.use(authController.protect);
+router.route("/updateMyPassword").patch(authController.updatePassword);
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteMe);
+
+router.use(authController.restrictTo("admin"));
 router
   .route("/")
   .get(userController.getAllUsers)
