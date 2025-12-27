81d56699e4f389ba232d76456e9f447bee07df6e
Anna Wójcik <aniawojcik894@gmail.com>
2025-12-27 14:12:24 +0100

    Downgrade Express to v4 for compatibility with security middleware (xss-clean, express-mongo-sanitize)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -32,12 +34,19 @@ app.use("/api", limiter); // limiter works only if URL contains /api
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: "10kb" }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 app.set("view engine", "pug"); // template engine - pug
 app.set("views", path.join(__dirname, "views"));
 
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.body);
   next();
 });
 
