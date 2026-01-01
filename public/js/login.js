4b291d4a2998b91ab87222b8c13d35645f16a285
Anna Wójcik <aniawojcik894@gmail.com>
2026-01-01 17:05:39 +0100

    Implement UI alerts and integrate with login

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,25 @@
 import axios from "axios";
+import { showAlert } from "./alerts";
 
 export const login = async (email, password) => {
   try {
     const res = await axios({
-      method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login', 
+      method: "POST",
+      url: "http://127.0.0.1:3000/api/v1/users/login",
       data: {
         email,
         password,
-      }
+      },
     });
-    if(res.data.status === "success") {
-      alert("Logged in successfully!");
+    if (res.data.status === "success") {
+      showAlert("success", "Logged in successfully!", 2);
       window.setTimeout(() => {
-        location.assign('/');
+        location.assign("/");
       }, 1500);
     }
-
   } catch (err) {
     const userError = document.querySelector(".user-error");
     userError.classList.remove("hidden");
-    alert(err.response.data.message);
+    showAlert("error", err.response.data.message, 2);
   }
-}
+};
